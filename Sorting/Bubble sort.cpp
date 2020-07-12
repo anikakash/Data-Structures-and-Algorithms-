d34decd535a596b4d55da8fe36249fe2fcaa0e41
anikakash <anik15-14210@diu.edu.bd>
2020-07-12 20:44:55 +0600

    Bubble Sort

--- Sorting/Bubble sort.cpp
+++ Sorting/Bubble sort.cpp
@@ -1,24 +1,31 @@
-#include<stdio.h>
-void bubble_sort(long list[], long n);
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long int ll;
+typedef double dl;
+#define pi= acose(-1);
+
+void bubble_sort(ll list[], ll n);
 int main()
 {
-    long array[100], n, c;
-    printf("Enter number of elements.\n");
+    ll array[100], c;
+    int n;
+    printf("Enter number of elements\n");
     scanf("%d", &n);
-    printf("Enter number of %ld integer.\n", n);
+    printf("Enter number of %d integer\n", n);
     for(c=0; c < n; c++){
-        scanf("%ld", &array[c]);
+        scanf("%lld", &array[c]);
     }
     bubble_sort(array, n);
     printf("Sorted list ascending order:\n");
     for(c=0; c < n; c++){
-        printf("%ld ", array[c]);
+        printf("%lld ", array[c]);
     }
     return 0;
 }
-void bubble_sort(long list[], long n)
+void bubble_sort(ll list[], ll n)
 {
-    long i,j,temp;
+    ll i,j,temp;
     int count =0;
     for(i=0; i<n;i++){
         for(j=0; j<n-i-1; j++){
